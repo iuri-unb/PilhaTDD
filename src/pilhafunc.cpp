836234158cedcaa0d93062c028e278fd3b06310f
Iuri Sousa Vieira <34162620+iuri-unb@users.noreply.github.com>
2020-10-15 12:31:14 -0300

    Adicionando Funções Pop e Push

--- src/pilhafunc.cpp
+++ src/pilhafunc.cpp
@@ -12,3 +12,38 @@ TipoPilha* criaPilha(void) {
     return monte;
 }
 
+int Push(TipoPilha *monte, TipodaPilha insercao) {
+/**
+ * @brief Insere um elemento na Pilha, contendo um valor (que é do tipo pilha)
+ * o tamanho da pilha é aumentado em 1
+ * @return Retorna 0 se a insercao foi feita ou -1 caso tenha falhado
+ * @param TipoPilha ponteiro(monte), TipoPilha insercao
+ */
+    //TipoPilha *monte = (TipoPilha *)malloc(sizeof(TipoPilha));
+    if( monte->tamanho == monte->tamanhomax ){
+        printf("Pilha Cheia! Nao foi feita a insercao!\n");
+        return -1;
+    } // end if
+    apontadorPilha *novo = (apontadorPilha *)malloc(sizeof(celulaPilha));
+    //* Setando a nova celula para ser inserida */
+    novo->valor = insercao;
+    novo->proximo = monte->topo;
+    monte->topo = novo;
+    monte->tamanho = monte->tamanho +1;
+    return 0;
+}
+
+apontadorPilha* Pop(TipoPilha *monte) {
+/**
+ * @brief Remove um elemento da Pilha e retorna esse mesmo elemento
+ *@return Retorna a celula da pilha que foi removida, com todos seus dados
+ * @param TipoPilha ponteiro(monte)
+ */
+    if ( monte->topo == NULL )
+        return NULL;  // Pilha vazia, nao removeu ninguem
+    apontadorPilha *novo = (apontadorPilha *)malloc(sizeof(celulaPilha));
+    monte->tamanho = monte->tamanho -1;
+    novo = monte->topo;
+    monte->topo = monte->topo->proximo;
+    return novo;
+} 
